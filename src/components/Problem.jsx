18523ca850a54925a6093271eaa077410ff5fd9a
kc-clintone <kayseeclintone@gmail.com>
2026-03-09 17:15:39 +0300

    chore: add problem section and initial data

--- src/components/Problem.jsx
+++ src/components/Problem.jsx
@@ -1,5 +1,29 @@
 export const Problem = () => {
   return (
-    <div>Problem</div>
+    <div className='flex flex-col items-center py-16 px-4 mx-auto min-h-screen bg-amber-50 min-w-screen'>
+      {/* TODO: Style this accordingly */}
+      <div className="container mx-auto">
+
+        <h2 className='text-4xl font-bold text-[#4B0082]'>The Problem</h2>
+        <p className='text-xl text-[#4B0082] max-w-3xl pt-5'>Investing can be overwhelming and time-consuming, especially for those who are new to the world of finance. Many people struggle to find reliable information and make informed decisions about their investments. This often leads to missed opportunities and suboptimal returns.</p>
+        <div className='flex items-center mt-10 gap-10'>
+          <div>
+            <p className='text-2xl font-bold text-[#4B0082] mt-10'>Information Overload</p>
+            <p className='text-lg text-[#4B0082] max-w-2xl pt-3'>With the vast amount of information available online, it can be difficult for investors to filter through the noise and find trustworthy sources.</p>
+          </div>
+
+                    <div>
+            <p className='text-2xl font-bold text-[#4B0082] mt-10'>Information Overload</p>
+            <p className='text-lg text-[#4B0082] max-w-2xl pt-3'>With the vast amount of information available online, it can be difficult for investors to filter through the noise and find trustworthy sources.</p>
+          </div>
+
+                    <div>
+            <p className='text-2xl font-bold text-[#4B0082] mt-10'>Information Overload</p>
+            <p className='text-lg text-[#4B0082] max-w-2xl pt-3'>With the vast amount of information available online, it can be difficult for investors to filter through the noise and find trustworthy sources.</p>
+          </div>
+        </div>
+      </div>
+
+    </div>
   )
 }
