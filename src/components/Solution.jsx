07dd3e1fcdc525426a4e6ca5eea83af5f5aa93d1
kc-clintone <kayseeclintone@gmail.com>
2026-03-09 17:25:31 +0300

    chore: add solutions section/component

--- src/components/Solution.jsx
+++ src/components/Solution.jsx
@@ -1,5 +1,11 @@
 export const Solution = () => {
   return (
-    <div>Solution</div>
+    <div className="mx-auto h-screen flex items-center justify-center container flex-col">
+      <h1 className="text-4xl font-bold text-[#4B0082] mb-6">Our Solution</h1>
+      <p className="text-xl text-[#4B0082] mb-8">Investa leverages cutting-edge AI technology to analyze market trends and provide personalized investment insights. Our platform empowers you to make informed decisions and grow your wealth effortlessly.</p>
+      <div>
+        <img src="/solutions.png" alt="" />
+      </div>
+    </div>
   )
 }
