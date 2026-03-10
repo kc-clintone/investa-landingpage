06897f9b68d9cf80a4c106701f368dfc4a1f7399
kc-clintone <kayseeclintone@gmail.com>
2026-03-10 14:37:44 +0300

    chore: add utils files

---
+++ utils.js
@@ -0,0 +1,8 @@
+export const navLinks = [
+    { name: "Home", href: "#" },
+    { name: "About Us", href: "#" },
+    { name: "Features", href: "#" },
+    { name: "Solutions", href: "#" },
+    { name: "F.A.Qs", href: "#" },
+    { name: "Contact", href: "#" },
+  ];
