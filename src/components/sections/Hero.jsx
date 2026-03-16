23e3900bcefa230874ae8deefcf169ade31e700d
kc-clintone <kayseeclintone@gmail.com>
2026-03-16 16:10:29 +0300

    fix: fix the styling for hero section

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -1,16 +1,21 @@
-import { Button } from "../ui/Button"
+import { Button } from "../ui/Button";
 
 export const Hero = () => {
   return (
-    <div className="mx-auto h-screen flex items-center justify-center bg-[#EEEDFF] w-2/3 container">
-      <div>
-        <h1 className="text-5xl font-bold text-[#4B0082] mb-6">Invest Smarter, Not Harder</h1>
-        <p className="text-xl text-[#4B0082] mb-8">Discover the power of AI-driven investment insights with Investa. Make informed decisions and grow your wealth effortlessly.</p>
-        <Button text="Suscibe To Our Newsletter" className="bg-blue-600" />
+    <div className="mx-auto h-screen flex items-center justify-center bg-[#EEEDFF] container gap-16 w-full -mt-16">
+      <div className="max-w-2xl ">
+        <h1 className="text-7xl font-bold text-[#140122] mb-6">
+          Invest Smarter, Not Harder
+        </h1>
+        <p className="text-xl text-[#60089b] mb-8">
+          Discover the power of AI-driven investment insights with Investa. Make
+          informed decisions and grow your wealth effortlessly.
+        </p>
+        <Button text="Subscibe To Our Newsletter" className="bg-[#1628D2] rounded-md" />
       </div>
       <div>
-        <img src="/hero.png" alt="" />
+        <img src="/hero.png" alt="" className="w-[512px] ml-20"/>
       </div>
     </div>
-  )
-}
+  );
+};
