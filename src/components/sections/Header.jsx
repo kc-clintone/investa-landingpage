459be6332aa432aa7e00152980023412588c0df9
kc-clintone <kayseeclintone@gmail.com>
2026-03-16 16:29:37 +0300

    fix: fix header styles

--- src/components/sections/Header.jsx
+++ src/components/sections/Header.jsx
@@ -4,16 +4,16 @@ import { navLinks } from "../../../utils.js"
 export const Header = () => {
   return (
     // TODO: Add responsive design and mobile menu
-    <div className="w-full h-16 flex items-center justify-between py-12 px-8 container mx-auto">
+    <div className="w-full h-16 flex items-center justify-between py-12 px-7 container max-w-355 mx-auto mt-5 sticky top-0 z-50">
         <div className="text-2xl font-bold text-[rgb(75,0,130)]">Investa</div>
-        <div className="flex items-center gap-2 bg-[#A1A1A1] px-1 py-1 rounded-full">
+        <div className="flex items-center gap-2 bg-[#A1A1A1]/30 px-1 py-1 rounded-full backdrop-blur-lg">
             {
               navLinks.map((link, index) => (
-                <a key={index} href={link.href} className="text-[#0b0311] hover:text-[#f8f8f8]/80 transition-colors duration-300 hover:bg-[#0011ff] rounded-full py-4 px-8">{link.name}</a>
+                <a key={index} href={link.href} className="text-[#400070] hover:text-[#f8f8f8]/80 transition-colors duration-300 hover:bg-[#0011ff] rounded-full py-4 px-8 text-[18px] font-semibold">{link.name}</a>
               ))
             }
         </div>
-        <Button text="Get Started Now" className="bg-[#4b0082] rounded-lg"/>
+        <Button text="Get Started Now" className="bg-[#1628D2] rounded-md"/>
     </div>
   )
 }
