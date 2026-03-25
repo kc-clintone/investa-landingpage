da306419977913bd6d066e216669e580b56b9d25
kc-clintone <kayseeclintone@gmail.com>
2026-03-26 00:02:21 +0300

    mod: modify components import and cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,30 @@
 import { Hero } from './components/sections/Hero'
-import { Problem } from './components/sections/Problem'
-import { Solution } from './components/sections/Solution'
-import { HowItWorks } from './components/sections/HowItWorks'
+import { Features } from './components/sections/Features'
+import { WhyUs } from './components/sections/WhyUs'
 import { CTA } from './components/sections/CTA'
 import { Layout } from './components/layout/Layout'
 import { FAQs } from './components/sections/FAQs'
+import { Testimonial } from './components/sections/Testimonial'
+import { Contact } from './components/sections/Contact'
 
 function App() {
   return (
-    <div className='flex flex-col min-h-screen w-screen bg-[#EEEDFF] overflow-x-hidden'>
+    <div className='flex flex-col min-h-screen bg-[#EEEDFF] overflow-x-hidden'>
     <Layout>
     {/* Hero */}
     <Hero />
-    {/* Problem */}
-    <Problem />
-    {/* Solution */}
-    <Solution />
-    {/* How it works */}
-    <HowItWorks />
+    {/* Features */}
+    <Features />
+    {/* WhyUs */}
+    <WhyUs />
     {/* FAQs */}
     <FAQs />
+    {/* Testimonials */}
+    <Testimonial />
     {/* CTA */}
     <CTA />
+    {/* Contact */}
+    <Contact/>
     </Layout>
     </div>
   )
