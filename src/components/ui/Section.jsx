a8b16300a21290d5f2a64882d872cc6e25646451
kc-clintone <kayseeclintone@gmail.com>
2026-03-26 00:04:08 +0300

    mod: maintain a reusable structure for the section

--- src/components/ui/Section.jsx
+++ src/components/ui/Section.jsx
@@ -1,13 +1,11 @@
 export const Section = ({
   children,
   className = "",
-  id,
   fullHeight = false,
 }) => {
   return (
     <section
-      id={id}
-      className={`w-full container mx-auto px-6 ${
+      className={`w-full my-10 container mx-auto px-6${
         fullHeight ? "min-h-screen flex items-center" : "py-24"
       } ${className}`}
     >
