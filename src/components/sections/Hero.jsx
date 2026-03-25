08d4ae3254b4d666eb2729739214c7d101086df9
kc-clintone <kayseeclintone@gmail.com>
2026-03-26 00:12:42 +0300

    mod: modified hero image to match design

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -3,18 +3,18 @@ import { Button } from "../ui/Button";
 
 export const Hero = () => {
   return (
-    <Section fullHeight className="flex justify-center gap-16">
-      <div className="max-w-2xl">
+    <Section fullHeight className="flex justify-center gap-20 -m-20">
+      <div className="max-w-2xl -mt-20">
         <h1 className="text-7xl font-bold text-[#140122] mb-6">
           Invest Smarter, Not Harder
         </h1>
 
-        <p className="text-xl text-[#60089b] mb-8">
+        <p className="text-xl text-[#350257] mb-8">
           Discover the power of AI-driven investment insights with Investa. Make
           informed decisions and grow your wealth effortlessly.
         </p>
 
-        <Button text="Subscribe To Our Newsletter" />
+        <Button text="Subscribe To Our Newsletter" className="bg-blue-700 rounded-sm px-10"/>
       </div>
 
       <img src="/hero.png" alt="hero" className="w-[512px]" />
